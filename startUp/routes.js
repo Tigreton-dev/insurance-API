34b78f0b231d6d1b93489e798d7aca6260c5ae57
javier <javier.san.mail@gmail.com>
2020-07-08 15:29:22 +0200

    client and policies routes done

--- startUp/routes.js
+++ startUp/routes.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const client = require("../routes/client");
+const policies = require("../routes/policies");
 
 module.exports = function (app) {
   app.use(express.json());
   app.use("/api/client", client);
+  app.use("/api/policies", policies);
 };
